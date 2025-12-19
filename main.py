32bf1edd3dd5c52e40caad726ed3e9cb0e303bca
konradroesler <konrad.rosler@gmail.com>
2025-12-19 13:05:18 +0100

    update

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import manim as mn
 
-class CreateCircle(mn.Scene):
+class AnkiCard(mn.Scene):
     def construct(self):
-        circle = mn.Circle()
-        circle.set_fill(mn.PINK, opacity=0.5)
-        self.play(mn.Create(circle))
+        line = r"Markov chain	$\mathbb{P}(X_n \in B | \mathcal{F}_m) = \mathbb{P}(X_n \in B | X_m)$"
+        tex1 = mn.Tex(r'Markov chain', font_size=144)
+        tex2 = mn.MathTex(r'\mathbb{P}(X_n \in B | \mathcal{F}_m) = \mathbb{P}(X_n \in B | X_m)')
+        group = mn.VGroup(tex1, tex2).arrange(
+                mn.DOWN,
+                aligned_edge=mn.LEFT,
+                buff=0.4
+        )
+
+        self.add(group)
